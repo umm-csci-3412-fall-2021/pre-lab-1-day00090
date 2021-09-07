ac5c3a0404817e89935afcc7cf4accfc077611e2
Kyle Day <day00090@spacewhale.morris.umn.edu>
2021-09-07 10:33:16 -0500

    Add Arguments to wrap_contents.sh
    This commit adds three variable arguments to wrap_contents.sh:
    + startFile = The starting file with the content
    + specification = The specifier for whether the contents should be
      wrapped as a header or footer
    + endFile = The resulting file with the wrapped content

---
+++ wrap_contents/wrap_contents.sh
@@ -0,0 +1,5 @@
+startFile=$1 #This variable contains the name of the file with the contents that need to be wrapped.
+specification=$2 #Specification is used to specify whether a header or footer is needed.
+endFile=$3 #This variable contains the name of the resulting file once the right information is wrapped.
+
+
