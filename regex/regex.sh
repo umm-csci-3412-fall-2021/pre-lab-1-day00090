005de40ef06ef34aece161e10af6e731b10bc2e3
Kyle Day <day00090@spacewhale.morris.umn.edu>
2021-09-07 21:09:02 -0500

    Add Final sed Call to regex.sh for Reformating Inputs
    This commit adds one final sed call to reformat r2_input.txt to
    r2_output.txt. In addition, it is commented, and it follows the
    shellcheck guidelines.

--- regex/regex.sh
+++ regex/regex.sh
@@ -4,4 +4,4 @@ awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "
 
 sed -En 's/\* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt #<Meanwhile, this expression uses sed to reformat the second input file to the output file using regex.
 
-
+sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt #Like the previous expression, this expression uses sed to reformat, but with the third input file to the output file using regex.
