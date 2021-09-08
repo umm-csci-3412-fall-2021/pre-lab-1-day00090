8b88162ab4e411fcae328661268a561ea6642f84
Kyle Day <day00090@spacewhale.morris.umn.edu>
2021-09-07 20:45:33 -0500

    Add sed Call for Formatting Second Input
    The most important change to regex.sh is that a sed call has been added
    to format r1_input.txt to r1_output.txt.
    
    The other two changes pertaining to the file add comments to each call
    and specify an interpreter for the script, per shellcheck guidelines.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,2 +1,7 @@
-awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt > r0_output.txt
+#!/bin/bash
+
+awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt > r0_output.txt #This expression uses awk to format the first i nput file to the output file using regex.
+
+sed -En 's/\* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt #<Meanwhile, this expression uses sed to reformat the second input file to the output file using regex.
+
 
